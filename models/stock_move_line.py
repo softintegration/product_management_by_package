66b00fc6cf670da0f5faf8b7dafa5e60a927373c
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2023-02-20 21:22:16 +0100

    Manufacturing order:Remove the required constraint from move lines result package

--- models/stock_move_line.py
+++ models/stock_move_line.py
@@ -4,13 +4,15 @@ from odoo import models, fields, api, _
 from odoo.exceptions import ValidationError
 from odoo.osv import expression
 
+IGNORED_DESTINATION_PACKAGE_TYPES = ('mrp_operation','outgoing')
+
 
 class StockMoveLine(models.Model):
     _inherit = "stock.move.line"
 
     managed_by_package = fields.Boolean(string='Managed by package', compute='_compute_managed_by_package')
-    package_id_required = fields.Boolean(compute='_compute_package_id_required',store=True)
-    result_package_id_required = fields.Boolean(compute='_compute_result_package_id_required',store=True)
+    package_id_required = fields.Boolean(compute='_compute_package_id_required', store=False)
+    result_package_id_required = fields.Boolean(compute='_compute_result_package_id_required', store=False)
 
     @api.depends('product_id')
     def _compute_managed_by_package(self):
@@ -20,12 +22,13 @@ class StockMoveLine(models.Model):
     @api.depends('managed_by_package', 'picking_id.picking_type_code')
     def _compute_package_id_required(self):
         for each in self:
-            each.package_id_required = each.managed_by_package and each.picking_id.picking_type_code in ('internal', 'outgoing')
+            each.package_id_required = each.managed_by_package and each.picking_id.picking_type_code in (
+            'internal', 'outgoing')
 
     @api.depends('managed_by_package')
     def _compute_result_package_id_required(self):
         for each in self:
-            each.result_package_id_required = each.managed_by_package
+            each.result_package_id_required = each.managed_by_package and each.picking_type_id.code not in IGNORED_DESTINATION_PACKAGE_TYPES
 
     def _action_done(self):
         res = super(StockMoveLine, self)._action_done()
@@ -33,16 +36,14 @@ class StockMoveLine(models.Model):
         return res
 
     def _check_packages(self):
-        for each in self.filtered(lambda ml:not self._deleted_line(ml)):
+        for each in self.filtered(lambda ml: not self._deleted_line(ml)):
             if each and each.package_id_required and not each.package_id:
-                raise ValidationError(_("Source package is required for product %s!")%each.product_id.display_name)
+                raise ValidationError(_("Source package is required for product %s!") % each.product_id.display_name)
             if each and each.result_package_id_required and not each.result_package_id:
-                raise ValidationError(_("Destination package is required for product %s!")%each.product_id.display_name)
+                raise ValidationError(
+                    _("Destination package is required for product %s!") % each.product_id.display_name)
 
     @api.model
-    def _deleted_line(self,move_line):
+    def _deleted_line(self, move_line):
         """ Detect he removed move line records during this transaction or concurrent one"""
         return not self.env.cache.contains(move_line, self) and not move_line.exists()
-
-
-
